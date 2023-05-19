fa897a3812b01454d1b0faf03347a89f576bb50f
Zarela <zarelag90@gmail.com>
2023-05-18 21:13:04 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Zarela G. & is{" "}
-        <a
-          href="https://github.com/zarela23/react-weather-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div>
+        Weather App
+        <footer>
+          This project was coded by Zarela G. & is{" "}
+          <a
+            href="https://github.com/zarela23/react-weather-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
