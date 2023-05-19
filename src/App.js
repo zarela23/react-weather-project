d685edc0184deeec58b71fbe8b2ec09d7f85694b
Zarela <zarelag90@gmail.com>
2023-05-18 20:53:41 -0400

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Zarela G. & is{" "}
+        <a
+          href="https://github.com/zarela23/react-weather-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
