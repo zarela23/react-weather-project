84578c99af016df62590b94180990ca5f3b8bc74
Zarela <zarelag90@gmail.com>
2023-05-24 10:55:11 -0400

    Updated forecast css

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -19,7 +19,7 @@ export default function WeatherForecast(props) {
   if (loaded) {
     return (
       <div className="WeatherForecast">
-        <div className="row mt-4">
+        <div className="row mt-5 text-center">
           {forecast.map(function (dailyForecast, index) {
             if (index < 5) {
               return (
